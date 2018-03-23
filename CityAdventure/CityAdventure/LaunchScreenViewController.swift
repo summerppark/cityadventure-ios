b83d83fc2876c028db2c59f49f2f5b1bb8eba0fe
jjy4880 <jjy48801@naver.com>
2018-03-23 17:57:17 +0900

    set LaunchScreen

--- CityAdventure/CityAdventure/LaunchScreenViewController.swift
+++ CityAdventure/CityAdventure/LaunchScreenViewController.swift
@@ -7,21 +7,41 @@
 //
 
 import UIKit
+import SwiftGifOrigin
+
 
 class LaunchScreenViewController: UIViewController {
     
     // gif
-    @IBOutlet weak var loadGifImage: UIImageView!
-    
-    
+    @IBOutlet weak var loadGifImage: UIImageView! {
+        didSet {
+            let gifFile = UIImage.gif(name: "loading")
+            loadGifImage.image = gifFile
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // navi hidden
+        self.navigationController?.navigationBar.isHidden = true
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3), execute: {
+            print("3초 후")
+            // 로그인 화면으로 옵셔널 체크 후 전환
+            if let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") {
+                self.navigationController?.pushViewController(loginVC, animated: true)
+            }
+        })
+    }
+    
     
 }
