ba138b6a184a8d1d11b38802aa580fdb2d0d12c5
jjy4880 <jjy48801@naver.com>
2018-04-15 00:28:54 +0900

    sideMenu data setting

--- CityAdventure/CityAdventure/Alert&Popup/RightSideMenuViewController.swift
+++ CityAdventure/CityAdventure/Alert&Popup/RightSideMenuViewController.swift
@@ -10,18 +10,62 @@ import UIKit
 
 class RightSideMenuViewController: BaseViewController {
     
+    
+    
+    
+    
     @IBOutlet weak var leftView: UIView!
     
     
     @IBOutlet weak var bgmSoundButtonHeight: NSLayoutConstraint!
     @IBOutlet weak var effectSoundButtonHeight: NSLayoutConstraint!
     
+    @IBOutlet weak var charImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
     
+    @IBOutlet weak var userAge: UILabel!
+    @IBOutlet weak var provinceCity: UILabel!
+    
+    // UserInfo
+    var userInfo: UserInfo.UserCharInfo?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         layoutCheck()
         addGesture()
+        dataSetting()
+    }
+    
+    
+    
+    // 사이드메뉴 데이터 셋팅.
+    func dataSetting() {
+        guard let info = DataManager.shared.userInfo?.userInfo else {
+            return
+        }
+        
+        // 캐릭터 이미지
+        if let avatar = info.ui_avatarNo {
+            charImage.image = super.charImages[avatar-1]
+        }
+        
+        // 나이를 계산.
+        if let birth = info.date_birth {
+            userAge.text = TimeFormatter().getCurrentAge(date_birth: birth)
+            
+            print(birth)
+        }
+        
+        // 사는 지역 보여주기.
+        if let cityNumber = info.ui_livingCity {
+        let temp = DataManager.shared.citynumbers[cityNumber-1]
+            provinceCity.text = "\(temp.cityName) \(temp.areaName)"
+        }
+        
+        
+        
+        
+        
     }
     
     func addGesture() {
--- CityAdventure/CityAdventure/Utils/TimeFormatter.swift
+++ CityAdventure/CityAdventure/Utils/TimeFormatter.swift
@@ -15,4 +15,21 @@ struct TimeFormatter {
         dateFormatter.dateFormat = "HH"
         return Int(dateFormatter.string(from: Date()))
     }
+    
+    
+    func getCurrentAge(date_birth: String) -> String {
+        
+        // 현재년도 2018
+        let year = Calendar.current.component(.year, from: Date())
+        // 앞글자 4자만짜른다 ex ) 1991
+        
+        let birth = Int(date_birth.prefix(4))
+        var result = 0
+        
+        if let myBirth = birth {
+           result = year - myBirth
+        }
+
+        return String(result)
+    }
 }
