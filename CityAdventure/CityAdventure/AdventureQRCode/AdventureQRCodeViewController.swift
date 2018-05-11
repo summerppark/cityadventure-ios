27f6372bbae71e3a3a1d39fd4679d4d27a6a0b2f
Jiyong <jjy48801@naver.com>
2018-05-11 09:19:50 +0900

    layout setting for device

--- CityAdventure/CityAdventure/AdventureQRCode/AdventureQRCodeViewController.swift
+++ CityAdventure/CityAdventure/AdventureQRCode/AdventureQRCodeViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class AdventureQRCodeViewController: BaseViewController {
     
+    // 파랑색 네모 높이
+    @IBOutlet weak var squareHeight: NSLayoutConstraint!
+    @IBOutlet weak var textTopConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        layoutCheck()
         
     }
     
+    func layoutCheck() {
+        //iPhoneX 는 네비게이션,스테이터스바 가 다른형식임
+        if Constants.DeviceType.IS_IPHONE_X {
+            
+            // iPhone X 일 때 레이아웃
+        }
+        
+        if Constants.DeviceType.IS_IPHONE_X || Constants.DeviceType.IS_IPHONE_6P {
+            squareHeight.constant = 340
+            textTopConstraint.constant = 78
+        }
+    
+    }
     
     @IBAction func tappedBackButton(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
