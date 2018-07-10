519eec249ab5c0e57505774661b8132b9584c4cf
jjy4880 <jjy48801@naver.com>
2018-07-10 12:35:52 +0900

    랜드마크 디테일 화면 API 연동

---
+++ CityAdventure/CityAdventure/AdventureQRCode/LandmarkSearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkSearchCell.swift
+//  CityAdventure
+//
+//  Created by Jiyong on 2018. 7. 10..
+//  Copyright © 2018년 bubu. All rights reserved.
+//
+
+import UIKit
+
+class LandmarkSearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CityAdventure/CityAdventure/Models/LandmarkDetail.swift
@@ -0,0 +1,9 @@
+//
+//  LandmarkDetail.swift
+//  CityAdventure
+//
+//  Created by Jiyong on 2018. 7. 10..
+//  Copyright © 2018년 bubu. All rights reserved.
+//
+
+import Foundation
